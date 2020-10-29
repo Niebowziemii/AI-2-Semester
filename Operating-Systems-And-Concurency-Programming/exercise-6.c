5b39256ef189918886f30ff4cdd4a603057f784b
Niebowziemii <59135705+Niebowziemii@users.noreply.github.com>
2020-10-29 19:14:26 +0100

    Create exercise-6.c

---
+++ Operating-Systems-And-Concurency-Programming/exercise-6.c
@@ -0,0 +1,115 @@
+/*MIT License
+Copyright (c) 2020: 
+Rafał Niebowziemii Stachowiak
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main(int argc,char** argv){
+    // pierwszy plik
+    off_t file_size;
+    char*buffer;
+    struct stat buf;
+    FILE* fd;
+
+    fd = fopen(argv[1],"rb");
+    if (fd==NULL){
+        fprintf(stderr,"File is has not been opened.");
+        return EXIT_FAILURE;
+    }
+
+    if((fstat(fileno(fd),&buf)!=0)||(!S_ISREG(buf.st_mode))) {
+        fprintf(stderr,"This program works only with regular files.");
+        return EXIT_FAILURE;
+    }
+
+    file_size=buf.st_size;
+    buffer = (char*)calloc(file_size,sizeof(char));
+    if(buffer==NULL){
+        fprintf(stderr,"Problem in allocating memory for the file.");
+        return EXIT_FAILURE;
+    }
+    fread(buffer,sizeof(char),file_size,fd);
+  // drugi plik
+    off_t file_size2;
+    char*buffer2;
+    FILE* fd2;
+
+    fd2 = fopen(argv[2],"rb");
+    if (fd2==NULL){
+        fprintf(stderr,"File is has not been opened.");
+        return EXIT_FAILURE;
+    }
+
+    if((fstat(fileno(fd2),&buf)!=0)||(!S_ISREG(buf.st_mode))) {
+        fprintf(stderr,"This program works only with regular files.");
+        return EXIT_FAILURE;
+    }
+
+    file_size2=buf.st_size;
+    buffer2 = (char*)calloc(file_size2,sizeof(char));
+    if(buffer2==NULL){
+        fprintf(stderr,"Problem in allocating memory for the file.");
+        return EXIT_FAILURE;
+    }
+    fread(buffer2,sizeof(char),file_size2,fd2);
+  // driver code
+  FILE* writed;
+  writed =fopen(argv[3],"wb");
+  int ind1=0,ind2=0;
+    while(1){
+        if(buffer[ind1]==10){
+            ind1++;
+        }
+        else if(buffer2[ind2]==10){
+            ind2++;
+        }
+        else if(buffer[ind1]&&buffer2[ind2]){
+            if(buffer[ind1]<=buffer2[ind2]){
+            fwrite(&buffer[ind1],1,1,writed);
+            fwrite("\n",1,1,writed);
+            ind1++;
+            }
+            else if(buffer[ind1]>buffer2[ind2]){
+            fwrite(&buffer2[ind2],1,1,writed);
+            fwrite("\n",1,1,writed);
+            ind2++;
+            }
+        }
+        else if(buffer[ind1]&&!buffer2[ind2]){
+            fwrite(&buffer[ind1],1,1,writed);
+            fwrite("\n",1,1,writed);
+            ind1++;
+        }
+        else if(buffer2[ind2]&&!buffer[ind1]){
+            fwrite(&buffer2[ind2],1,1,writed);
+            fwrite("\n",1,1,writed);
+            ind2++;
+        }
+        else if(!buffer2[ind2]&&!buffer[ind1])
+        break;
+    }
+    
+    fclose(writed);
+    fclose(fd);
+    fclose(fd2);
+    return EXIT_SUCCESS;
+}
